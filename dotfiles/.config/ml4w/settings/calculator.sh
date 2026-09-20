69a7f105488d7dfbc5abdeac4902132ae07ffa14
Chachi <w.jiaqi004@gmail.com>
2026-09-21 01:50:04 +0200

    hypr: rofi -> walker

--- dotfiles/.config/ml4w/settings/calculator.sh
+++ dotfiles/.config/ml4w/settings/calculator.sh
@@ -1,1 +1,2 @@
-pkill rofi || rofi -show calc -modi calc -no-show-match -no-sort -no-persist-history -calc-command "echo '{result}' | wl-copy"
+# pkill rofi || rofi -show calc -modi calc -no-show-match -no-sort -no-persist-history -calc-command "echo '{result}' | wl-copy"
+/home/chachi/.mydotfiles/dotfiles/dotfiles/.config/walker/launch.sh --provider=calc
